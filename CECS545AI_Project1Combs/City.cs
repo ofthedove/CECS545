a3c9d89f8ab708a6135fd20f06f98d9b617c11df
Andrew Combs <andrewcombs13@gmail.com>
2016-09-19 22:13:44 -0400

    Start groundwork for project 3
    Convert city to class, improve OOP of everything, stuff

---
+++ CECS545AI_Project1Combs/City.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* KeyValuePair - City
+ * {
+ *      int id
+ *      tuple - data
+ *      {
+ *          double xCoord
+ *          double yCoord
+ *          list - connections
+ *          {
+ *              tuple - connection
+ *              {
+ *                  int id
+ *                  double distance
+ *              }
+ *          }
+ *      }
+ * }
+ * */
+
+namespace CECS545AI_Project1Combs
+{
+    class City
+    {
+        private int id;
+        private double xCoord;
+        private double yCoord;
+        private List<Edge> edges;
+        private bool locked;
+
+        public int ID
+        {
+            get
+            {
+                return id;
+            }
+        }
+        public int X
+        {
+            get
+            {
+                return xCoord;
+            }
+        }
+        public int Y
+        {
+            get
+            {
+                return yCoord;
+            }
+        }
+        public List<Edge> Edges
+        {
+            get
+            {
+                return edges;
+            }
+        }
+
+        public City(int idIn, double xCoordIn, double yCoordIn)
+        {
+
+        }
+
+        public void Lock()
+        {
+            locked = true;
+        }
+
+        public bool AddEdge(City city)
+        {
+            if(!locked)
+            {
+                Edge edge = new Edge();
+                return AddEdge(edge);
+            }
+            return false;
+        }
+
+        public bool AddEdge(Edge edge)
+        {
+            if (!locked)
+            {
+                edges.Add(edge);
+                return true;
+            }
+            return false;
+        }
+
+        public void SelectEdge(City city)
+        {
+            Edge edge = new Edge(this, city);
+            SelectEdge(edge);
+        }
+
+        public void SelectEdge(Edge edge)
+        {
+            
+        }
+
+        public void DeselectEdge(City city)
+        {
+            Edge edge = new Edge();
+            DeselectEdge(edge);
+        }
+
+        public void DeselectEdge(Edge edge)
+        {
+
+        }
+    }
+}
---
+++ CECS545AI_Project1Combs/Edge.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CECS545AI_Project1Combs
+{
+    /// <summary>
+    /// Edge from City 1 to City 2.
+    /// </summary>
+    class Edge
+    {
+        private City city1;
+        private City city2;
+        private double distance; // weight
+        private bool selected;
+
+        public City City1
+        {
+            get { return city1;  }
+        }
+        public City City2
+        {
+            get { return city2; }
+        }
+        public double Distance
+        {
+            get { return distance; }
+        }
+        public bool Selected
+        {
+            get { return selected; }
+            set { selected = value; }
+        }
+
+        public Edge(City city1In, City city2In)
+        {
+            city1 = city1In;
+            city2 = city2In;
+            distance = DistanceBetween(city1, city2);
+            selected = false;
+        }
+
+        public void Select()
+        {
+            Selected = true;
+        }
+
+        public void Deselect()
+        {
+            Selected = false;
+        }
+
+        private static double DistanceBetween(City city1, City city2)
+        {
+            return Math.Sqrt(Math.Pow((city2.X - city1.X), 2) + Math.Pow((city2.Y - city1.Y), 2));
+        }
+    }
+}
