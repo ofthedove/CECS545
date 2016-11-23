33e650df943bfc078484488f50c8d5a9675308b1
Andrew Combs <andrewcombs13@gmail.com>
2016-11-23 11:16:55 -0500

    Started a new version of project 4. Named it something weird by mistake

---
+++ AIProject4Nes/AIProject4Nes/Log.cs
@@ -0,0 +1,135 @@
+﻿using GAF;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cecs545FinalProject
+{
+    public class Log
+    {
+        private ClickOMania.Board board;
+        private List<GenerationData> logData;
+
+        public ClickOMania.Board OriginalBoard { get { return board;  } }
+        
+        public int Length { get { return logData.Count;  } }
+
+        public class GenerationData
+        {
+            private double genNum;
+            private double avgFitness;
+            private double stdDevFit;
+
+            private Chromosome mostFit;
+            private Chromosome leastFit;
+            private Chromosome WocSln;
+
+            public double GenNum { get { return genNum; } }
+            public double MaxFitness { get { return mostFit.Fitness; } }
+            public double MinFitness { get { return leastFit.Fitness; } }
+            public double AvgFitness { get { return avgFitness; } }
+            public double StdDevFit { get { return stdDevFit; } }
+            public double WocFitness { get { return WocSln.Fitness; } }
+
+            public int[] MostFitSolution { get { return ChromToSolution(mostFit); } }
+            public int[] LeastFitSolution { get { return ChromToSolution(leastFit); } }
+            public int[] WocSlnSolution { get { return ChromToSolution(WocSln); } }
+
+            public string Blurb
+            {
+                get
+                {
+                    return string.Format("Gen {0,3:###} : WoC {1,5:0.000} | Max {2,5:0.000} | Avg {3,5:0.000} | SDv {4,5:0.000}", genNum, WocFitness, MaxFitness, avgFitness, stdDevFit);
+                }
+            }
+
+            private GenerationData(int genNumIn, double avgFitIn, double stdDevFitIn, Chromosome mostFitIn, Chromosome leastFitIn, Chromosome WocSlnIn)
+            {
+                genNum = genNumIn;
+                avgFitness = avgFitIn;
+                stdDevFit = stdDevFitIn;
+
+                mostFit = mostFitIn;
+                leastFit = leastFitIn;
+                WocSln = WocSlnIn;
+            }
+
+            public static GenerationData GenDataFromPopulation(int genNumIn, Population pop, Chromosome WocSlnIn)
+            {
+                double avgFitness = pop.AverageFitness;
+
+                double stdDevFit = 0;
+                double runningSum = 0;
+                foreach(Chromosome chrom in pop.Solutions)
+                {
+                    runningSum += System.Math.Pow(chrom.Fitness - avgFitness, 2);
+                }
+                stdDevFit = runningSum / (double)pop.PopulationSize;
+                stdDevFit = System.Math.Sqrt(stdDevFit);
+
+                Chromosome mostFit = pop.GetTop(1)[0];
+                Chromosome leastFit = pop.GetBottom(1)[0];
+
+                return new GenerationData(genNumIn, avgFitness, stdDevFit, mostFit, leastFit, WocSlnIn);
+            }
+
+            public static int[] ChromToSolution(Chromosome chrom)
+            {
+                int[] arr = new int[25];
+                int i = 0;
+
+                foreach (Gene gene in chrom)
+                {
+                    arr[i] = (int)gene.ObjectValue;
+                    i++;
+                }
+
+                return arr;
+            }
+        }
+
+        public Log(ClickOMania.Board brd)
+        {
+            board = brd;
+            logData = new List<GenerationData>();
+        }
+
+        public void Write(GenerationData item)
+        {
+            logData.Add(item);
+        }
+
+        public string ReadShort(int index)
+        {
+            if (index < 0 || index >= Length)
+            {
+                throw new ArgumentException("Specified index does not exist in log");
+            }
+
+            return logData[index].Blurb;
+        }
+
+        public GenerationData ReadFull(int index)
+        {
+            if (index < 0 || index >= Length)
+            {
+                throw new ArgumentException("Specified index does not exist in log");
+            }
+
+            return logData[index];
+        }
+
+        public void SaveBrief(string filePath)
+        {
+            StreamWriter writer = new StreamWriter(filePath);
+            for(int i = 0; i < this.Length; i++)
+            {
+                writer.WriteLine(this.ReadShort(i));
+            }
+            writer.Close();
+        }
+    }
+}
---
+++ AIProject4Nes/AIProject4Nes/ResultNavigator.xaml.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace cecs545FinalProject
+{
+    /// <summary>
+    /// Interaction logic for ResultNavigator.xaml
+    /// </summary>
+    public partial class ResultNavigator : Window
+    {
+        private Log log;
+
+        private List<Bitmap> wocImgList;
+        private List<Bitmap> bstImgList;
+        private List<Bitmap> lstImgList;
+
+        public ResultNavigator(Log logIn)
+        {
+            InitializeComponent();
+
+            log = logIn;
+
+            List<string> items = new List<string>();
+            for(int i = 0; i < log.Length; i++)
+            {
+                items.Add(log.ReadShort(i));
+            }
+            genListBox.ItemsSource = items;
+
+            // Select the last generation in the generation list
+            genListBox.SelectedIndex = genListBox.Items.Count - 1;
+        }
+
+        private void genListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (genListBox.SelectedItem != null)
+            {
+                // Retrieve the data relating to the selected generation
+                int genIndex = genListBox.SelectedIndex;
+                Log.GenerationData selectedGen = log.ReadFull(genIndex);
+
+                // Update the general info section
+                generationValueLabel.Content = selectedGen.GenNum;
+                wocFitnessValueLabel.Content = String.Format("{0:0.000}", selectedGen.WocFitness);
+                maxFitnessValueLabel.Content = String.Format("{0:0.000}", selectedGen.MaxFitness);
+                minFitnessValueLabel.Content = String.Format("{0:0.000}", selectedGen.MinFitness);
+
+                // ---- Update the board viewer ----
+                // Clear the image viewers
+                WoCSlnImage.Source = null;
+                BestFitImage.Source = null;
+                LeastFitImage.Source = null;
+
+                // Generate the images
+                wocImgList = ClickOMania.GenerateImageList(selectedGen.WocSlnSolution, log.OriginalBoard.GetBoardAsArray());
+                bstImgList = ClickOMania.GenerateImageList(selectedGen.MostFitSolution, log.OriginalBoard.GetBoardAsArray());
+                lstImgList = ClickOMania.GenerateImageList(selectedGen.LeastFitSolution, log.OriginalBoard.GetBoardAsArray());
+
+                // Reset the sliders to 0
+                WoCSlnSlider.Value = 0;
+                BestFitSlider.Value = 0;
+                LeastFitSlider.Value = 0;
+
+                // Update the slider max values
+                WoCSlnSlider.Maximum = wocImgList.Count - 1;
+                BestFitSlider.Maximum = bstImgList.Count - 1;
+                LeastFitSlider.Maximum = lstImgList.Count - 1;
+
+                // Set all sliders to their max value
+                WoCSlnSlider.Value = WoCSlnSlider.Maximum;
+                BestFitSlider.Value = BestFitSlider.Maximum;
+                LeastFitSlider.Value = LeastFitSlider.Maximum;
+            }
+        }
+
+        private void WoCSlnSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            var slider = sender as Slider;
+            int value = (int)slider.Value;
+            BitmapImage bitmapImage = new BitmapImage();
+            using (MemoryStream memory = new MemoryStream())
+            {
+                wocImgList[value].Save(memory, ImageFormat.Png);
+                memory.Position = 0;
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+            }
+            WoCSlnImage.Source = bitmapImage;
+        }
+
+        private void BestFitSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            var slider = sender as Slider;
+            int value = (int)slider.Value;
+            BitmapImage bitmapImage = new BitmapImage();
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bstImgList[value].Save(memory, ImageFormat.Png);
+                memory.Position = 0;
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+            }
+            BestFitImage.Source = bitmapImage;
+        }
+
+        private void LeastFitSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            var slider = sender as Slider;
+            int value = (int)slider.Value;
+            BitmapImage bitmapImage = new BitmapImage();
+            using (MemoryStream memory = new MemoryStream())
+            {
+                lstImgList[value].Save(memory, ImageFormat.Png);
+                memory.Position = 0;
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+            }
+            LeastFitImage.Source = bitmapImage;
+        }
+    }
+}
