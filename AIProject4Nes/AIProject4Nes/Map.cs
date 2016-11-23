3eb43b68d10e6c0ef04758d2f496c00b9c2dad52
Andrew Combs <andrewcombs13@gmail.com>
2016-11-23 12:27:15 -0500

    Add map from old project, add MapBuilder
    MapBuilder helps build maps to guarentee that a map, once made, can't be
    altered

---
+++ AIProject4Nes/AIProject4Nes/Map.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIProject4Nes
+{
+    public class Map
+    {
+        private List<City> Cities;
+
+        public int NumCities
+        {
+            get { return Cities.Count; }
+        }
+
+        public Map(List<City> citiesIn)
+        {
+            Cities = citiesIn;
+        }
+
+        public City GetCityByID(int id)
+        {
+            foreach (City city in Cities)
+            {
+                if (city.ID == id)
+                {
+                    return city;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ AIProject4Nes/AIProject4Nes/MapBuilder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIProject4Nes
+{
+    class MapBuilder
+    {
+        protected List<City> Cities;
+        
+        public MapBuilder()
+        {
+            Cities = new List<City>();
+        }
+
+        public bool AddCity(City city)
+        {
+            if (Cities.Contains(city))
+            {
+                return false;
+            }
+            else
+            {
+                Cities.Add(city);
+                return true;
+            }
+        }
+
+        public Map ToMap()
+        {
+            return new Map(Cities); ;
+        }
+    }
+}
