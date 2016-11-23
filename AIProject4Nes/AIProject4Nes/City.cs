6f79510bc9bc23d0a848f5b406c9f6e0d9e7fd10
Andrew Combs <andrewcombs13@gmail.com>
2016-11-23 12:26:33 -0500

    Add City from old project

---
+++ AIProject4Nes/AIProject4Nes/City.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIProject4Nes
+{
+    class City : IComparable
+    {
+        private int id;
+        private double xCoord;
+        private double yCoord;
+
+        public int ID { get { return id; } }
+        public double X { get { return xCoord; } }
+        public double Y { get { return yCoord; } }
+
+        public City(int idIn, double xCoordIn, double yCoordIn)
+        {
+            id = idIn;
+            xCoord = xCoordIn;
+            yCoord = yCoordIn;
+        }
+
+        public int CompareTo(object obj)
+        {
+            City c = (City)obj;
+            return Math.Abs(c.ID.CompareTo(ID));
+        }
+    }
+}
